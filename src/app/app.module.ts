83e9dea7a492a0b0f866f62e50f37db3ef742013
perkles <andyrodrigo2@gmail.com>
2022-07-01 14:03:51 -0300

    componete to-do-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule,
